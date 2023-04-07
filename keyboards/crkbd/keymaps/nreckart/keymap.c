5dab4fae548ac219dc3f0ab5ef3080775c76c0aa
Nathan Reckart <nreckart@gmail.com>
2023-04-07 15:51:38 -0400

    Tweaks for Corne

--- keyboards/crkbd/keymaps/nreckart/keymap.c
+++ keyboards/crkbd/keymaps/nreckart/keymap.c
@@ -44,7 +44,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define SPCSHFT LSFT_T(KC_SPC)
 #define WS_UP   LCTL(LGUI(KC_UP))
 #define WS_DWN  LCTL(LGUI(KC_DOWN))
-#define SFT_GRV LSFT(KC_GRV)
+#define SFT_GRV LSFT_T(KC_GRV)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB ,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC ,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,  SEMIMOV, KC_QUOT,
+      KC_ESC ,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, SEMIMOV, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      SFT_GRV,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      SFT_GRV,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_DEL ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           TD_ACBS, CTLBSPC, KC_LGUI,    ENTNUM,  SPCSHFT,  KC_TAB 
                                       //`--------------------------'  `--------------------------'
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NUMBERS] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_F12,  KC_EQL , KC_LCBR, KC_RCBR, KC_MINS, KC_PLUS,                      KC_LEFT, KC_LPRN, KC_RPRN,  WS_UP , WS_DWN , KC_F11,
+      KC_F12,  KC_EQL , KC_LCBR, KC_RCBR, KC_MINS, KC_PLUS,                      XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, KC_F11,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_F12 ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_AT , KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,                       KC_GRV, XXXXXXX, XXXXXXX,  WS_DWN, XXXXXXX, XXXXXXX,
+      KC_LSFT,KC_COLON, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,                       KC_GRV,KC_COLON, _______, _______, _______, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -75,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MOVE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U,                      KC_PGUP, KC_HOME,  KC_UP , KC_END , XXXXXXX, XXXXXXX,
+       KC_TAB, KC_EXLM, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U,                      KC_PGUP, KC_HOME,  KC_UP , KC_END , XXXXXXX, QK_BOOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,  CTL_A , KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT, KC_BSLS, KC_GRV,
+      KC_LCTL,  CTL_A , KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, RGB_TOG, RGB_MOD,RGB_RMOD, XXXXXXX, KC_BSPC,                      XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, KC_PIPE, KC_TILD,
+      KC_LSFT, RGB_MOD,RGB_RMOD, WS_DWN ,  WS_UP , RGB_TOG,                      XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
